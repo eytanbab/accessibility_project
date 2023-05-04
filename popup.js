8cc4a117f775faa7863d60811f085a2ac5eca51e
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-05-04 16:06:39 +0300

    added comments

--- popup.js
+++ popup.js
@@ -1,8 +1,28 @@
+/**
+ * Executes the code inside the event listener when the DOM content is loaded.
+ */
 document.addEventListener('DOMContentLoaded', function () {
+  /**
+   * Gets the HTML button element with the ID "increase-font" and assigns it to a variable.
+   * @type {HTMLElement}
+   */
   var increaseFontButton = document.getElementById('increase-font');
+
+  /**
+   * Gets the HTML button element with the ID "decrease-font" and assigns it to a variable.
+   * @type {HTMLElement}
+   */
   var decreaseFontButton = document.getElementById('decrease-font');
+
+  /**
+   * Gets the HTML button element with the ID "reset-font" and assigns it to a variable.
+   * @type {HTMLElement}
+   */
   var resetFontButton = document.getElementById('reset-font');
 
+  /**
+   * Adds a click event listener to the "increase-font" button that increases the font size of the active tab's body by 2px.
+   */
   increaseFontButton.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.executeScript(tabs[0].id, {
@@ -11,6 +31,9 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
+  /**
+   * Adds a click event listener to the "decrease-font" button that decreases the font size of the active tab's body by 2px.
+   */
   decreaseFontButton.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.executeScript(tabs[0].id, {
@@ -19,6 +42,9 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
+  /**
+   * Adds a click event listener to the "reset-font" button that resets the font size of the active tab's body to its default size.
+   */
   resetFontButton.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.executeScript(tabs[0].id, {
