26d437478143cdd96f55c9992080f3e7c0e67802
eytanbab <73233191+eytanbab@users.noreply.github.com>
2023-05-24 10:48:58 +0300

    added bold font functionality

--- popup.js
+++ popup.js
@@ -3,6 +3,12 @@
  */
 document.addEventListener('DOMContentLoaded', function () {
   /**
+   * Gets the HTML button element with the ID "bold-font" and assigns it to a variable.
+   * @type {HTMLElement}
+   */
+  var boldFontButton = document.getElementById('bold-font');
+
+  /**
    * Gets the HTML button element with the ID "increase-font" and assigns it to a variable.
    * @type {HTMLElement}
    */
@@ -59,6 +65,17 @@ document.addEventListener('DOMContentLoaded', function () {
   /**
    * Adds a click event listener to the "increase-font" button that increases the font size of the active tab's body by 2px.
    */
+  boldFontButton.addEventListener('click', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.executeScript(tabs[0].id, {
+        code: "document.body.style.fontWeight = 'bold';",
+      });
+    });
+  });
+
+  /**
+   * Adds a click event listener to the "increase-font" button that increases the font size of the active tab's body by 2px.
+   */
   increaseFontButton.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.executeScript(tabs[0].id, {
@@ -84,7 +101,7 @@ document.addEventListener('DOMContentLoaded', function () {
   resetFontButton.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.executeScript(tabs[0].id, {
-        code: "document.body.style.fontSize = '';",
+        code: "document.body.style.fontSize = ''; document.body.style.fontWeight = '';",
       });
     });
   });
